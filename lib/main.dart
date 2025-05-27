86bfee80e42be9f46ca28c5636b795ea6ff264ba
salama <imammuhammed797@gmail.com>
2025-05-27 15:14:58 +0300

    create fcm tocken

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 import 'package:furniswap/data/api_services/api_sevice.dart';
 import 'package:furniswap/data/repository/auth_repoImpl.dart';
 import 'package:furniswap/data/repository/auth_repo.dart';
@@ -11,8 +14,33 @@ import 'package:furniswap/presentation/manager/review/cubit/review_cubit.dart';
 import 'package:furniswap/presentation/manager/signup/sign_up_cubit.dart';
 import 'package:furniswap/presentation/screens/splash_screen.dart';
 
+/// ✅ handler بيشتغل لما يجيلك إشعار والتطبيق مقفول
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('💤 Background Message: ${message.messageId}');
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // ✅ تهيئة Firebase
+  await Firebase.initializeApp();
+
+  // ✅ تسجيل الـ background handler
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // ✅ طباعة الـ FCM Token
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print('✅ FCM Token: $fcmToken');
+
+  // ✅ استقبال الرسائل في foreground
+  // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  //   print("📩 Foreground Message: ${message.notification?.title}");
+  // });
+
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    print("🚀 App opened from Notification: ${message.notification?.title}");
+  });
 
   final dio = Dio();
   final apiService = ApiService(dio);
